3cbf9f4e37c26e55eeb9820fd21783cc672993df
Mark Callaghan <mdcallag@gmail.com>
2015-05-28 15:58:53 -0700

    log errors

--- rall.sh
+++ rall.sh
@@ -29,15 +29,15 @@ echo Load at $( date )
 ( iostat -kx 2 >& o.$engine.io.load & )
 ( vmstat 2 >& o.$engine.vm.load & )
 
-./${engine}_bench bench_config.ini.load > o.${engine}.load
+./${engine}_bench bench_config.ini.load > o.${engine}.load 2> o.${engine}.err.load
 mv ${logpath}_* o.${engine}.log.load
 du -hs ${dbpath}* > o.$engine.sz.load
 
 grep elapsed o.$engine.load | head -3
 grep elapsed o.$engine.load | head -3 > o.res
 
-killall vmstat
 killall iostat
+killall vmstat
 
 fi
 
@@ -47,14 +47,14 @@ for p in 1 n ; do
 ( iostat -kx 2 >& o.$engine.io.$t.$p & )
 ( vmstat 2 >& o.$engine.vm.$t.$p & )
 
-./${engine}_bench bench_config.ini.$t.$p > o.${engine}.$t.$p 
+./${engine}_bench bench_config.ini.$t.$p > o.${engine}.$t.$p 2> o.${engine}.err.$t.$p
 mv ${logpath}_* o.${engine}.log.$t.$p
 du -hs ${dbpath}* > o.$engine.sz.$t.$p
 
 echo $t $p $( grep ops\/sec\,  o.$engine.$t.$p | grep -v total ) 
 
-killall vmstat
 killall iostat
+killall vmstat
 
 done
 done
